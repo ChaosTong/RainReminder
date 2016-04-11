56bb2d727783fe518b816117ea3ee32292a20870
Chaos丶Lonely <easyulife@gmail.com>
2016-04-11 14:31:31 +0800

    search done.

---
+++ RainReminder/CityListCell.swift
@@ -0,0 +1,24 @@
+//
+//  CityListCell.swift
+//  RainReminder
+//
+//  Created by 童超 on 16/4/11.
+//  Copyright © 2016年 ChaosTong. All rights reserved.
+//
+
+import UIKit
+
+class CityListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
