e80d6ec68c5ca80ec05e431fded04e293ccd8ff9
ChaosTong <easyulife@gmail.com>
2020-07-04 23:51:14 +0800

    new branch for mac

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by tongchao on 2020/7/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/RainReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RainReminderApp.swift
+//  Shared
+//
+//  Created by tongchao on 2020/7/4.
+//
+
+import SwiftUI
+
+@main
+struct RainReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
